e5b1e4652c838029edf9ca3ad87f7f06f10e3559
Marc Christophe <m@xtof.net>
2015-01-08 11:15:39 -0800

    smart-mode-line, among other things...killing global-visual-line-mode

--- init.el
+++ init.el
@@ -18,6 +18,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-babel-load-languages (quote ((emacs-lisp . t) (scheme . t))))
-)
+ '(custom-safe-themes
+   (quote
+    ("e56f1b1c1daec5dbddc50abd00fcd00f6ce4079f4a7f66052cf16d96412a09a9" default)))
+ '(org-babel-load-languages (quote ((emacs-lisp . t) (scheme . t)))))
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-mode-line-clock ((t (:background "grey75" :foreground "red" :box (:line-width -1 :style released-button)))) t))

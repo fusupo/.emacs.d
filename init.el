a9a1d9897cb9208daf2e1c447cde1dd9be15d61a
Marc Christophe <m@xtof.net>
2015-01-11 15:15:07 -0800

    adding graphical support in babel, customizations to super-mode-line

--- init.el
+++ init.el
@@ -13,6 +13,12 @@
   (org-babel-load-file filename))
 (mapc #'load-config config-files)
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-mode-line-clock ((t (:background "grey75" :foreground "red" :box (:line-width -1 :style released-button)))) t))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -20,17 +26,4 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("e56f1b1c1daec5dbddc50abd00fcd00f6ce4079f4a7f66052cf16d96412a09a9" default)))
- '(org-babel-load-languages (quote ((emacs-lisp . t) (scheme . t)))))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-mode-line-clock ((t (:background "grey75" :foreground "red" :box (:line-width -1 :style released-button)))) t)
- '(sml/folder ((t (:inherit sml/global :background "#091f2e" :foreground "gray40" :weight normal))))
- '(sml/modes ((t (:inherit sml/global :background "#0a3749" :foreground "gray65"))))
- '(sml/position-percentage ((t (:inherit sml/prefix :background "#0a3749" :foreground "gray100" :weight normal))))
- '(sml/vc ((t (:inherit sml/git :background "#0a3749" :foreground "SlateBlue2"))))
- '(sml/vc-edited ((t (:inherit sml/prefix :background "#0a3749" :foreground "OrangeRed1")))))
+    ("e56f1b1c1daec5dbddc50abd00fcd00f6ce4079f4a7f66052cf16d96412a09a9" default))))

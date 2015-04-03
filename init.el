c3849e26aaf5744b661b3e17bef8d2bb8ed3768d
Marc Christophe <m@xtof.net>
2015-04-03 15:45:50 -0700

    whittling down effective smartparens config minor adjustments on org2blog and j2mode configs

--- init.el
+++ init.el
@@ -20,6 +20,7 @@
  ;; If there is more than one, they won't work right.
  '(js2-function-call ((t (:inherit default :foreground "DarkSeaGreen4"))))
  '(org-mode-line-clock ((t (:background "grey75" :foreground "red" :box (:line-width -1 :style released-button)))) t))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -28,5 +29,4 @@
  '(cursor-type (quote bar))
  '(custom-safe-themes
    (quote
-    ("e56f1b1c1daec5dbddc50abd00fcd00f6ce4079f4a7f66052cf16d96412a09a9" default)))
- '(js2-strict-inconsistent-return-warning nil))
+    ("30a8a5a9099e000f5d4dbfb2d6706e0a94d56620320ce1071eede5481f77d312" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "e56f1b1c1daec5dbddc50abd00fcd00f6ce4079f4a7f66052cf16d96412a09a9" default))))

bfe8ca72269c32922280896594ba02812686e30a
Marc Christophe <m@xtof.net>
2015-09-11 11:40:31 -0700

    adding neotree font face

--- init.el
+++ init.el
@@ -19,6 +19,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(js2-function-call ((t (:inherit default :foreground "DarkSeaGreen4"))))
+ '(neo-root-dir-face ((t (:background "#0c2938" :foreground "#ffd746"))))
  '(org-mode-line-clock ((t (:background "grey75" :foreground "red" :box (:line-width -1 :style released-button)))) t))
 
 (custom-set-variables
@@ -29,5 +30,5 @@
  '(cursor-type (quote bar))
  '(custom-safe-themes
    (quote
-    ("30a8a5a9099e000f5d4dbfb2d6706e0a94d56620320ce1071eede5481f77d312" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "e56f1b1c1daec5dbddc50abd00fcd00f6ce4079f4a7f66052cf16d96412a09a9" default)))
+    ("2d7e4feac4eeef3f0610bf6b155f613f372b056a2caae30a361947eab5074716" "30a8a5a9099e000f5d4dbfb2d6706e0a94d56620320ce1071eede5481f77d312" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "e56f1b1c1daec5dbddc50abd00fcd00f6ce4079f4a7f66052cf16d96412a09a9" default)))
  '(timesheet-invoice-number 104))

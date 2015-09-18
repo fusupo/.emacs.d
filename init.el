0f573e4c71c0ae974f1d4606e1ec23deec4aab31
Marc Christophe <m@xtof.net>
2015-09-17 23:31:23 -0700

    removing some styles from init.el

--- init.el
+++ init.el
@@ -18,8 +18,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(js2-function-call ((t (:inherit default :foreground "DarkSeaGreen4"))))
- '(org-mode-line-clock ((t (:background "grey75" :foreground "red" :box (:line-width -1 :style released-button)))) t))
+ '(js2-function-call ((t (:inherit default :foreground "DarkSeaGreen4")))))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
